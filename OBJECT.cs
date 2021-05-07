22944941477e46b2066ea9b23c7500e871115bd0
UNITY21047 <unity21047@gmail.com>
2021-05-07 15:11:28 +0000

    Major Update:
    Condensed delgate functionality->2 classes. TODO: work with interfaces.

--- OBJECT.cs
+++ OBJECT.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 
 namespace _2021_04_22_Raylib_ImGUI_Tiled_Study
 {
-    public class OBJECT
+    public class OBJECT : SKILL_EVENTS
     {
         String FILE_NAME;
         Dictionary<Vector2, int> TEXTURE_DATA_PAIR; //texture_position, texture_name
         float GLOBAL_POSITION;
         int LIST_POSITION;
-        //List<> OBJECT_SKILLS;
+        //Dictionary<SKILL_EVENT_ARGUMENTS, String> OBJECT_SKILLS; //SKILL_EVENT_ARGUMENTS, SKILL_NAME
         //List<> OBJECT_EVENTS;
         bool MULTIPLE_TEXTURES;
 
-        public OBJECT()
+        public OBJECT(String file_name, Dictionary<Vector2, int> texture_data_pair, float global_postion, int list_position, bool multiple_textures)
         {
             
         }
---
+++ SKILL_EVENTS.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace _2021_04_22_Raylib_ImGUI_Tiled_Study
+{
+    public class SKILL_EVENTS
+    {
+        
+    }
+}
