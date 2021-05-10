597439feab343ca6dbfabb0a58d27d43c58bdd74
UNITY21047 <unity21047@gmail.com>
2021-05-10 19:06:59 +0000

    Major Update:
    Started work on test tilemap.

---
+++ data/assets/tilesets/game_tiles/tileset_test_00.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tileset_test_00" tilewidth="32" tileheight="32" tilecount="840" columns="35">
+ <image source="tileset_test_00.png" width="1123" height="794"/>
+</tileset>
