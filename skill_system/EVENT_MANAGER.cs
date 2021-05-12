859bfb6b3bfc40874469fe88d5c444bb7994d0c6
UNITY21047 <unity21047@gmail.com>
2021-05-12 17:36:13 +0000

    Minor Update:
    Organizing the skill system.

--- skill_system/EVENT_MANAGER.cs
+++ skill_system/EVENT_MANAGER.cs
@@ -4,6 +4,7 @@ using Raylib_cs;
 
 namespace _2021_04_22_Raylib_ImGUI_Tiled_Study
 {
+    //Rework.
     public delegate void EVENT_HANDLER(object sender, EventArgs e);
 
     public class EVENT_MANAGER
