08c1d618d3964225128a9201a1d8639d31e6232c
UNITY21047 <unity21047@gmail.com>
2021-05-20 14:37:33 -0500

    Major Update:
    To many updates.

---
+++ data/assets/tilesets/game_tiles/game_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="game_tiles" tilewidth="32" tileheight="32" tilecount="840" columns="35">
+ <image source="../test_tiles/test_tileset.svg" width="1122" height="793"/>
+</tileset>
---
+++ data/assets/tilesets/game_tiles/game_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="game_tileset" tilewidth="32" tileheight="32" tilecount="840" columns="35">
+ <image source="game_tileset.svg" width="1123" height="794"/>
+</tileset>
