4cc6c4bff394685ebabd5c253b844cf40c38d86d
UNITY21047 <unity21047@gmail.com>
2021-05-20 14:44:50 -0500

    Major Update:
    Creating a test file for entity creation.

---
+++ src/entities/test_entity/test.cs
@@ -0,0 +1,16 @@
+using System;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _2021_04_22_Raylib_ImGUI_Tiled_Study
+{
+    public class test : entity
+    {
+        public test()
+        {
+            
+        }
+    }
+}
